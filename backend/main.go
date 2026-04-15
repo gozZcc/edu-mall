a1cc163d17da5f0a81fbad5a06420f71e414200e
gozZcc <ccz19927522992@163.com>
2026-04-15 21:45:53 +0800

    Revert "Feature/router"

--- backend/main.go
+++ backend/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"backend/config"
-	"backend/router"
-	"fmt"
 )
 
 func main() {
-	c, err := config.Init()
+	_, err := config.Init()
 	if err != nil {
 		panic("基础配置启动失败")
 	}
-	// 注册路由
-	re := router.Init()
-	// 启动服务
-	re.Run(fmt.Sprintf(":%v", c.Server.HttpPort))
 }
